698cb4a0cd94d99b946eb1f3a76421fc16229a04
Noah Gullo <noahgulloingram@gmail.com>
2026-07-27 08:58:22 -0700

    feat: add message details and links for every route

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,22 +5,33 @@ const messages = [
     {
         text: "Hi there!",
         user: "Amando",
-        added: new Date()
+        added: new Date(),
+        id: crypto.randomUUID(),
     },
     {
         text: "Hello World!",
         user: "Charles",
-        added: new Date()
+        added: new Date(),
+        id: crypto.randomUUID(),
     }
 ];
 
 indexRouter.get("/", (req, res) => res.render("../views/index.ejs", {messages: messages}));
+
+indexRouter.post("/messages/:messageID", (req, res) => {
+    const targetMessage = messages.filter(msg => msg.id === req.params.messageID)[0];
+    console.log(targetMessage);
+    res.render("../views/message.ejs", {message: targetMessage});
+})
+
 indexRouter.get("/new", (req, res) => res.render("../views/form.ejs"))
 indexRouter.post("/new", (req, res) => {
     messages.push({ 
         text: req.body.messageInput, 
         user: req.body.authorInput, 
-        added: new Date() });
+        added: new Date(),
+        id: crypto.randomUUID(),
+    });
     res.redirect("/");
 });
 
