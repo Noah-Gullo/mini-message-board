bdce161c92850855144da9e6e44bb6733fff9e78
Noah Gullo <noahgulloingram@gmail.com>
2026-07-27 07:44:59 -0700

    feat: render messages

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,6 +1,19 @@
 const { Router } = require("express")
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res) => res.render("../views/index.ejs"));
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    }
+];
+
+indexRouter.get("/", (req, res) => res.render("../views/index.ejs", {messages: messages}));
 
 module.exports = indexRouter;
